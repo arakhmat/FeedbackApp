bad3037e31af2e5a35e0993560c755ee0b502131
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-04-20 01:12:52 -0400

    Run with heroku

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, resp) => {
-    res.send('')
+    resp.send('Hi There')
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
