5f21c58773a394a6014c4128ed9031c28363c830
Akhmed Rakhmati <akhmed.rakhmati@gmail.com>
2018-04-26 00:32:40 -0400

    Added prod env. Started creating front-end

--- services/passport.js
+++ services/passport.js
@@ -22,17 +22,15 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleID: profile.id }).then(existingUser => {
-                if (existingUser) {
-                    // already have a record
-                    done(null, existingUser);
-                } else {
-                    new User({ googleID: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            });
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleID: profile.id });
+
+            if (existingUser) {
+                return done(null, existingUser);
+            }
+
+            const user = await new User({ googleID: profile.id }).save();
+            done(null, user);
         }
     )
 );
